ae9ebb10b43466399a2c1f712371f2e4f53e75ba
Patryk Szymik <patryk.szymik01@wp.pl>
2023-08-19 22:31:07 +0200

    try to make slider of heroes

--- src/components/HeroSmallCard.jsx
+++ src/components/HeroSmallCard.jsx
@@ -6,116 +6,104 @@ import MainButton from './MainButton';
 const HeroSmallCard = ({ name, image, desc }) => {
   return (
     <Box
-      className="slide-container"
+      className="card"
       sx={{
-        background: 'grey',
-        maxWidth: '1320px',
-        width: '100%',
-        padding: '40px 0',
+        borderRadius: '25px',
+        backgroundColor: '#fff',
       }}
     >
-      <Box className="slide-content" sx={{ margin: '0 40px' }}>
-        <Box className="card-wrapper">
-          <Box
-            className="card"
-            sx={{
-              width: '25%',
-              borderRadius: '25px',
+      <Box
+        className="image-content"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          position: 'relative',
+          padding: '2.19rem 0.88rem',
+        }}
+      >
+        <Box
+          className="overlay"
+          sx={{
+            position: 'absolute',
+            left: '0',
+            top: '0',
+            height: '100%',
+            width: '100%',
+            backgroundColor: '#4070f4',
+            borderRadius: '25px 25px 0 25px',
+
+            '::before, ::after': {
+              content: '""',
+              position: 'absolute',
+              right: '0',
+              bottom: '-40px',
+              height: '40px',
+              width: '40px',
+              backgroundColor: '#4070f4',
+            },
+
+            '::after': {
+              width: '41px',
+              borderRadius: '0 25px 0 0',
               backgroundColor: '#fff',
+            },
+          }}
+        />
+        <Box
+          className="card-image"
+          sx={{
+            position: 'relative',
+            width: '150px',
+            height: '150px',
+            borderRadius: '50%',
+            backgroundColor: '#fff',
+            padding: '3px',
+          }}
+        >
+          <img
+            src={image}
+            alt={name}
+            style={{
+              height: '100%',
+              width: '100%',
+              objectFit: 'cover',
+              borderRadius: '50%',
+              border: '4px solid #4070f4',
             }}
-          >
-            <Box
-              className="image-content"
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                position: 'relative',
-                padding: '35px 14px',
-              }}
-            >
-              <Box
-                className="overlay"
-                sx={{
-                  position: 'absolute',
-                  left: '0',
-                  top: '0',
-                  height: '100%',
-                  width: '100%',
-                  backgroundColor: '#4070f4',
-                  borderRadius: '25px 25px 0 25px',
-
-                  '::before, ::after': {
-                    content: '""',
-                    position: 'absolute',
-                    right: '0',
-                    bottom: '-40px',
-                    height: '40px',
-                    width: '40px',
-                    backgroundColor: '#4070f4',
-                  },
-
-                  '::after': {
-                    borderRadius: '0 25px 0 0',
-                    backgroundColor: '#fff',
-                  },
-                }}
-              ></Box>
-
-              <Box
-                className="card-image"
-                sx={{
-                  position: 'relative',
-                  width: '150px',
-                  height: '150px',
-                  borderRadius: '50%',
-                  backgroundColor: '#fff',
-                  padding: '3px',
-                }}
-              >
-                <img
-                  src={image}
-                  alt={name}
-                  className="card-img"
-                  style={{
-                    height: '100%',
-                    width: '100%',
-                    objectFit: 'cover',
-                    borderRadius: '50%',
-                    border: '4px solid #4070f4',
-                  }}
-                />
-              </Box>
-            </Box>
-            <Box
-              className="text-content"
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                padding: '0.625rem 0.875rem',
-              }}
-            >
-              <Typography
-                variant="h3"
-                sx={{ fontSize: '1.125rem', color: '#333' }}
-              >
-                {name}
-              </Typography>
-              <Typography
-                variant="body1"
-                sx={{
-                  fontSize: '0.875rem',
-                  color: '#707070',
-                  textAlign: 'center',
-                }}
-              >
-                {desc}
-              </Typography>
-              <MainButton text="More" link={''} style={''} />
-            </Box>
-          </Box>
+          />
         </Box>
+      </Box>
+      <Box
+        className="text-content"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '0.625rem 0.875rem',
+        }}
+      >
+        <Typography
+          variant="h3"
+          sx={{ fontSize: '1.5rem', color: '#333', padding: '1rem 0' }}
+        >
+          {name}
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{
+            fontSize: '0.875rem',
+            color: '#707070',
+            textAlign: 'center',
+          }}
+        >
+          {desc}
+        </Typography>
+        <MainButton
+          text="More"
+          link="/"
+          style={{ margin: '2rem 0 0.5rem 0' }}
+        />
       </Box>
     </Box>
   );
--- src/pages/HeroesPage.jsx
+++ src/pages/HeroesPage.jsx
@@ -1,5 +1,11 @@
 import { Box, Stack } from '@mui/material';
 import { useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 import HeroSmallCard from '../components/HeroSmallCard';
 
@@ -16,11 +22,70 @@ const HeroesPage = () => {
           justifyContent: 'center',
         }}
       >
-        <HeroSmallCard
-          name={cardItems[34].title}
-          image={cardItems[34].cover_url}
-          desc={cardItems[3].overview}
-        />
+        <Box
+          sx={{
+            background: 'grey',
+            maxWidth: '1320px',
+            width: '100%',
+            padding: '2.5rem',
+          }}
+        >
+          <Box sx={{ padding: '2.82rem 1.25rem' }}>
+            <Box>
+              <Swiper
+                slidesPerView={3}
+                spaceBetween={25}
+                loop={true}
+                centeredSlides={true}
+                fadeEffect={true} //?
+                grabCursor={true}
+                pagination={{
+                  clickable: true,
+                  dynamicBullets: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+              >
+                <SwiperSlide>
+                  <HeroSmallCard
+                    name={cardItems[34].title}
+                    image={cardItems[34].cover_url}
+                    desc={cardItems[3].overview}
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <HeroSmallCard
+                    name={cardItems[34].title}
+                    image={cardItems[34].cover_url}
+                    desc={cardItems[3].overview}
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <HeroSmallCard
+                    name={cardItems[34].title}
+                    image={cardItems[34].cover_url}
+                    desc={cardItems[3].overview}
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <HeroSmallCard
+                    name={cardItems[34].title}
+                    image={cardItems[34].cover_url}
+                    desc={cardItems[3].overview}
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <HeroSmallCard
+                    name={cardItems[34].title}
+                    image={cardItems[34].cover_url}
+                    desc={cardItems[3].overview}
+                  />
+                </SwiperSlide>
+              </Swiper>
+            </Box>
+          </Box>
+        </Box>
       </Box>
     </Stack>
   );
