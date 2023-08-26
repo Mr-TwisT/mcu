2ec6dbfe0bf8e52b3bc49251257d532430a27e67
Patryk Szymik <patryk.szymik01@wp.pl>
2023-08-26 17:50:32 +0200

    change location of data folder

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App.jsx';
 
-import { store } from './assets/data/store';
+import { store } from './data/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
